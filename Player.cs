b63559845ccb827ad22c0f1524506c2b2e25079d
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2019-02-06 15:09:09 -0500

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GremlinQuestV1._1
+{
+    class Player
+    {
+        public Rectangle playerRectangle = new Rectangle();
+        public Point playerPoint = new Point();
+        public Point PlayerMove (Rectangle pRectangle, Point pP)
+        {
+            playerPoint = pP;
+            if (Keyboard.IsKeyDown(Key.W))
+            {
+                pP.Y = pP.Y - 10;
+            }
+            return pP;
+        }
+
+        public void GeneratePlayer(Canvas canvas, Point pP)
+        {
+            playerPoint = pP;
+            playerRectangle.Height = 50;
+            playerRectangle.Width = 50;
+            playerRectangle.Fill = Brushes.Red;
+            canvas.Children.Add(playerRectangle);
+            Canvas.SetLeft(playerRectangle, playerPoint.X);
+            Canvas.SetTop(playerRectangle, playerPoint.Y);
+        }
+
+    }
+}
