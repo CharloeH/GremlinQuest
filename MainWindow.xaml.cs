e41d82af3bd1a751ac4f2d9b3094075db7580990
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2019-02-06 14:44:57 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GremlinQuestV1._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Menu menu;
+        public MainWindow()
+        {
+            menu = new Menu();
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Testbox();
+        }
+
+        private void btnCredits_Click(object sender, RoutedEventArgs e)
+        {
+            menu.CreditsPage(btnPlay, btnCredits, btnBack, txtCredits);
+            Testbox();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            menu.MainPage(btnPlay, btnCredits, btnBack, txtCredits);
+            Testbox();
+        }
+        private void Testbox()
+        {
+            MessageBox.Show("test");
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GremlinQuestV1._1
+{
+    class Menu
+    {
+        public void MainPage(Button btnCredits, Button btnPlay, Button btnBack, TextBlock txtCredits)
+        {
+            btnCredits.Visibility = Visibility.Visible;
+            btnPlay.Visibility = Visibility.Visible;
+            txtCredits.Visibility = Visibility.Hidden;
+            btnBack.Visibility = Visibility.Hidden;
+        }
+        public void CreditsPage(Button btnCredits, Button btnPlay, Button btnBack, TextBlock txtCredits)
+        {
+            btnCredits.Visibility = Visibility.Hidden;
+            btnPlay.Visibility = Visibility.Hidden;
+            txtCredits.Visibility = Visibility.Visible;
+            btnBack.Visibility = Visibility.Visible;
+        }
+        public void Testbox()
+        {
+            //MessageBox.Show("test");
+        }
+    }
+    }
